da8edf9ebc035d99d3fffe4873ee8dfb4a71236c
Nitheshwaran <76184804+Nithesh005@users.noreply.github.com>
2025-08-19 18:50:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from time import sleep
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World! This is a Flask app."
+
+@app.route('/hello')
+def hello_v1():
+    return "Hello, I'm here .. !"
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
